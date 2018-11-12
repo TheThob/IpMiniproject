91fad36b863800b1a3deb1661032e84c8be3d962
Thobias <36990151+TheThob@users.noreply.github.com>
2018-11-12 17:52:55 +0100

    trying to rotate

--- RotateImage.py
+++ RotateImage.py
@@ -6,28 +6,32 @@ class rotator:
     height = 0
     width = 0
     angle = 90
+    img = cv2.imread('lena.jpg',0)
 
     def __init__(self):
         pass
 
-    def showImg(arg,name, self):
-        img = cv2.imread(arg,0)
-        cv2.imshow(name, img)
-        self.width,self.height = img.shape
+    def showImg(name, self):
+        cv2.imshow(name, self.img)
+        self.width,self.height = self.img.shape
+
     def printWH(self):
         print(self.width)
         print(self.height)
+
     def rotate(self):
         for x in range(self.width):
             for y in range(self.height):
                 x = x*math.cos(self.angle)-y*math.sin(self.angle)
                 y = x*math.sin(self.angle)+y*math.cos(self.angle)
+        self.img.shape = self.width,self.height
+        cv2.imshow('rotated', self.img.shape)
 
 def main():
-    rotator.showImg('lena.jpg','normal', rotator)
+    rotator.showImg('normal', rotator)
     rotator.printWH(rotator)
     rotator.rotate(rotator)
-    rotator.showImg('lena.jpg','rotated', rotator)
+    #rotator.showImg('rotated', rotator)
     cv2.waitKey(0)
     cv2.destroyAllWindows
 
