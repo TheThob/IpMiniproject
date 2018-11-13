1350e164fe6f189a4f65ad52fd995d3767ea4b5d
TheThob <36990151+TheThob@users.noreply.github.com>
2018-11-13 15:54:58 +0100

    trying

--- RotateImage.py
+++ RotateImage.py
@@ -6,8 +6,9 @@ class rotator:
 
     height = 0
     width = 0
-    angle = 90
+    angle = 360
     img = cv2.imread('lena.jpg',0)
+
 
     def __init__(self):
         pass
@@ -21,12 +22,23 @@ class rotator:
         print(self.height)
 
     def rotate(self):
+        empty = np.zeros((self.height*2,self.width*2),dtype="uint8")
         for x in range(self.width):
             for y in range(self.height):
-                x = x*math.cos(self.angle)-y*math.sin(self.angle)
-                y = x*math.sin(self.angle)+y*math.cos(self.angle)
-        self.img.shape = self.width,self.height
-        cv2.imshow('rotated', self.img.shape)
+                if x<219 or y < 219:
+                    alpha = 1 * math.cos(self.angle)
+                    beta = 1 * math.sin(self.angle)
+                    temp = self.img[y,x]
+                    #xr = x*math.cos(self.angle)+y*math.sin(self.angle)
+                    #yr =  -x*math.sin(self.angle)+y*math.cos(self.angle)
+
+                    xr = alpha+beta+ (1-alpha)*(self.width/2)-beta*(self.height/2)
+                    yr = -beta + alpha + beta*(self.width/2)+(1-alpha)*(self.height/2)
+
+                    empty[int(yr),int(xr)] = temp
+
+
+        cv2.imshow('rotated', empty)
 
 def main():
     rotator.showImg('normal', rotator)
