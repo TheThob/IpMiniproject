ca3aa8da6f7888922ed4bb35507fef1b20ac450c
Thobias <36990151+TheThob@users.noreply.github.com>
2018-11-21 20:34:46 +0100

    Done

--- RotateImage.py
+++ RotateImage.py
@@ -12,7 +12,6 @@ class rotator:
     img = cv2.imread('lena.jpg',0)
     width,height = img.shape
 
-
     def showImg(name, self):
         cv2.imshow(name, self.img)
         self.img = np.pad(self.img, (self.height) ,'constant', constant_values=0)
@@ -27,56 +26,34 @@ class rotator:
         emptyB = np.zeros((self.width,self.height),dtype="uint8")
         emptyBB = np.zeros((self.width,self.height),dtype="uint8")
 
-
         for i in range(self.width):
             for j in range(self.height):
 
-
-                temp = self.img[i,j]
-
-                ix = i-self.x
-            #    cosX = int(ix*math.cos(self.radians))
-            #    sinX = int(ix*math.sin(self.radians))
-
-
-                jy = j-self.y
-            #    sinY = int(jy*math.sin(self.radians))
-            #    cosY = int(jy*math.cos(self.radians))
-
                 #forward mapping
-                #xf = (cosX-sinY)+self.x
-                #yf = (sinX+cosY)+self.x
-
                 xf = int((i-self.x)*math.cos(self.radians)-(j-self.y)*math.sin(self.radians))+self.x
                 yf = int((i-self.x)*math.sin(self.radians)+(j-self.y)*math.cos(self.radians))+self.x
 
 
                 #backward map of the forward mapped picture
-                xb = (xf-self.x)*math.cos(self.radians)+(yf-self.y)*math.sin(self.radians)+self.x
-                yb = -(xf-self.x)*math.sin(self.radians)+(yf-self.y)*math.cos(self.radians)+self.x
-
+                xfb = (xf-self.x)*math.cos(self.radians)+(yf-self.y)*math.sin(self.radians)+self.x
+                yfb = -(xf-self.x)*math.sin(self.radians)+(yf-self.y)*math.cos(self.radians)+self.x
 
                 #backward map of the original image
-                xbb = int((i-self.x)*math.cos(self.radians)+(j-self.y)*math.sin(self.radians)+self.x)
-                ybb = int(-((i-self.x)*math.sin(self.radians))+((j-self.y)*math.cos(self.radians))+self.x)
+                xb = int((i-self.x)*math.cos(self.radians)+(j-self.y)*math.sin(self.radians)+self.x)
+                yb = int(-((i-self.x)*math.sin(self.radians))+((j-self.y)*math.cos(self.radians))+self.x)
 
-
-            #    xbb = cosX+sinY+self.x
-            #    ybb = -sinX+cosY+self.x
-
-
-                if xf < 660 and yf < 660 and xf>0 and yf > 0:
-                    emptyF[int(xf),int(yf)] = temp
+                if xf < self.width and yf < self.height and xf>0 and yf > 0:
+                    emptyF[i,j] = self.img[int(xf),int(yf)]
                 else:
                     pass
 
-                if xb < 660 and yb < 660 and xb>0 and yb > 0:
-                    emptyB[int(xb),int(yb)] = temp
+                if xfb <= 660 and yfb <= 660 and xfb>=0 and yfb >= 0:
+                    emptyB[i,j] = self.img[int(xfb),int(yfb)]
                 else:
                     pass
 
-                if xbb < 660 and ybb < 660 and xbb>0 and ybb > 0:
-                    emptyBB[(xbb),(ybb)] = temp
+                if xb < self.width and yb < self.height and xb>=0 and yb >= 0:
+                    emptyBB[i,j] = self.img[int(xb),int(yb)]
                 else:
                     pass
 
