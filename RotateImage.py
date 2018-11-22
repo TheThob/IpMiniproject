af21232935fb30c886810b5997b66dc5ccf3233a
Thobias <36990151+TheThob@users.noreply.github.com>
2018-11-22 15:05:38 +0100

    method split

--- RotateImage.py
+++ RotateImage.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class rotator:
 
-    angle = 40.0
+    angle = 30.0
     x = 330
     y = 330
 
@@ -24,46 +24,7 @@ class rotator:
     def getImage(self):
         return self.img
 
-    def rotate(self):
-        emptyF = np.zeros((self.width,self.height),dtype="uint8")
-        emptyB = np.zeros((self.width,self.height),dtype="uint8")
-        emptyBB = np.zeros((self.width,self.height),dtype="uint8")
-
-        for i in range(self.width):
-            for j in range(self.height):
-
-                #forward mapping
-                xf = int((i-self.x)*math.cos(self.radians)-(j-self.y)*math.sin(self.radians))+self.x
-                yf = int((i-self.x)*math.sin(self.radians)+(j-self.y)*math.cos(self.radians))+self.x
-
-
-                #backward map of the forward mapped picture
-                xfb = (xf-self.x)*math.cos(self.radians)+(yf-self.y)*math.sin(self.radians)+self.x
-                yfb = -(xf-self.x)*math.sin(self.radians)+(yf-self.y)*math.cos(self.radians)+self.x
-
-                #backward map of the original image
-                xb = int((i-self.x)*math.cos(self.radians)+(j-self.y)*math.sin(self.radians)+self.x)
-                yb = int(-((i-self.x)*math.sin(self.radians))+((j-self.y)*math.cos(self.radians))+self.x)
-
-                if xf < self.width and yf < self.height and xf>0 and yf > 0:
-                    emptyF[i,j] = self.img[int(xf),int(yf)]
-                else:
-                    pass
-
-                if xfb <= self.width and yfb <= self.height and xfb>=0 and yfb >= 0:
-                    emptyB[i,j] = self.img[int(xfb),int(yfb)]
-                else:
-                    pass
-
-                if xb < self.width and yb < self.height and xb>=0 and yb >= 0:
-                    emptyBB[i,j] = self.img[int(xb),int(yb)]
-                else:
-                    pass
-
-        cv2.imshow('Forward', emptyF)
-        cv2.imshow('Forward Backward', emptyB)
-        cv2.imshow('Backward', emptyBB)
-
+    #Rotates an image using forward mapping
     def forward(self, img):
         empty = np.zeros((self.width,self.height),dtype="uint8")
 
@@ -81,6 +42,7 @@ class rotator:
 
         return empty
 
+    #Rotates an image using backward mapping
     def backward(self, img):
         empty = np.zeros((self.width,self.height),dtype="uint8")
 
@@ -98,6 +60,7 @@ class rotator:
 
         return empty
 
+    #Rotates an image using forward mapping and then rotates it back using backward mapping
     def backwardForward(self, img):
         empty = np.zeros((self.width,self.height),dtype="uint8")
 
@@ -121,7 +84,6 @@ class rotator:
 def main():
     rotator.showImg('normal', rotator)
     rotator.printWH(rotator)
-    #rotator.rotate(rotator)
     cv2.imshow('forward', rotator.forward(rotator, rotator.getImage(rotator)))
     cv2.imshow('backward', rotator.backward(rotator, rotator.getImage(rotator)))
     cv2.imshow('foward image backward', rotator.backwardForward(rotator, rotator.getImage(rotator)))
